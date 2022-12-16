225cfa4c0a366a764e55f1f82d0b4d040a4aaba1
Benedikt Willi <benedikt.willi@unibas.ch>
2022-12-16 16:32:50 +0100

    Startet working on ajax flagging feature.
    Added json response and data varibles in template.
    
    I do have to provide a template_list_js value.
    
    Updated templates to create an id for the flags element, updated js to replace html from response.
    
    Forgot to rebuild js.
    
    Fixed typo

--- django_comments_ink/static/django_comments_ink/js/index.js
+++ django_comments_ink/static/django_comments_ink/js/index.js
@@ -1,12 +1,15 @@
 import { init_comments } from "./comments.js";
 import { init_reactions } from "./reactions.js";
+import { init_flagging } from "./flagging.js";
 
 window.djCommentsInk = {
     init_comments: init_comments,
-    init_reactions: init_reactions
+    init_reactions: init_reactions,
+    init_flagging: init_flagging
 };
 
 window.addEventListener("DOMContentLoaded", (_) => {
     init_comments();
     init_reactions();
+    init_flagging();
 });
